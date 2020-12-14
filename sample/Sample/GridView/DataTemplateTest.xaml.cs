7d18c4adf517ff6e73b5ed5562297509c1e9857e
sung-su.kim <sung-su.kim@samsung.com>
2020-12-14 17:30:05 +0900

    add test 2

--- sample/Sample/GridView/DataTemplateTest.xaml.cs
+++ sample/Sample/GridView/DataTemplateTest.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,7 @@ namespace Sample.GridView
         public DataTemplateTest()
         {
             InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.TizenSpecific.ItemsView.SetFocusedItemScrollPosition(collectionview, ScrollToPosition.Center);
         }
     }
 }
